1c54aa8f547d90549da48e79239392225d6b55fa
Camilo-Suarez98 <camilo.suarez85@gmail.com>
2025-06-20 14:29:19 -0500

    complete header

--- wp-content/themes/sm-prueba/functions.php
+++ wp-content/themes/sm-prueba/functions.php
@@ -20,3 +20,17 @@ function slider_plugin_scripts() {
     wp_enqueue_script('mi-custom-js', get_stylesheet_directory_uri() . '/js/custom.js', ['swiper-js'], null, true);
 }
 add_action('wp_enqueue_scripts', 'slider_plugin_scripts');
+
+function font_family_resource() {
+  wp_enqueue_style(
+    'google-fonts-montserrat',
+    'https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap',
+    false
+  );
+
+  wp_enqueue_style(
+    'mi-tema-style',
+    get_stylesheet_uri()
+  );
+}
+add_action('wp_enqueue_scripts', 'font_family_resource');
--- wp-content/themes/sm-prueba/header.php
+++ wp-content/themes/sm-prueba/header.php
@@ -3,37 +3,63 @@
 <head>
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title><?php bloginfo('name'); ?> | <?php wp_title(); ?></title>
+  <title><?php bloginfo('name'); ?> | <?php wp_title(); ?> Prueba Tecnica</title>
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 <header class="site-header">
   <div class="logo">
-    <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png" alt="Logo">
+    <img class="logo-img" src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png" alt="Logo">
+    <form class="search-form-mobile" method="get" action="<?php echo home_url('/'); ?>">
+      <button class="search">
+        <img class="search-icon" src="<?php echo get_stylesheet_directory_uri(); ?>/images/search-mobile.png" alt="Search">
+      </button>
+    </form>
   </div>
 
-  <nav class="main-menu">
-    <?php
-    wp_nav_menu([
-      'theme_location' => 'menu_principal',
-      'container' => false,
-      'menu_class' => 'menu'
-    ]);
-    ?>
-  </nav>
-
-  <div class="header-extras">
-    <form class="search-form" role="search" method="get" action="<?php echo home_url('/'); ?>">
-      <input type="search" name="s" placeholder="Buscar">
-      <button type="submit">🔍</button>
-    </form>
-
-    <div class="social-icons">
-      <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icon-fb.png" alt="Facebook"></a>
-      <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icon-li.png" alt="LinkedIn"></a>
-      <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icon-flag.png" alt="English"></a>
+  <div class="navigation-complete">
+    <div class="nav-options">
+      <nav class="main-menu">
+        <a href="#">Trabaja con nosotros</a>
+        <a href="#">Grupo Nutresa</a>
+        <a href="#">Contáctanos</a>
+      </nav>
+    
+      <div class="header-extras">
+        <form class="search-form" role="search" method="get" action="<?php echo home_url('/'); ?>">
+          <input type="search" name="s" placeholder="Buscar">
+          <button type="submit">🔍</button>
+        </form>
+        
+        <div class="social-icons">
+          <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icon-facebook.png" alt="Facebook"></a>
+          <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/icon-linkedin.png" alt="LinkedIn"></a>
+          <a href="#"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/england-flag.png" alt="English"></a>
+        </div>
+        
+        <button class="menu-toggle">☰</button>
+      </div>
     </div>
 
-    <button class="menu-toggle">☰</button>
+    <div class="second-line-menu">
+      <nav class="main-menu">
+        <a href="#">
+          Nuestra Compañía
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/down-row.png" alt="Row">
+        </a>
+        <a href="#">Organización Consciente</a>
+        <a href="#">Marcas</a>
+        <a href="#">
+          Internacional
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/down-row.png" alt="Row">
+        </a>
+        <a href="#">Negocios Industriales</a>
+        <a href="#">
+          Fomento Cacaotero
+          <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/down-row.png" alt="Row">
+        </a>
+        <a href="#">Sostenibilidad</a>
+      </nav>
+    </div>
   </div>
 </header>
